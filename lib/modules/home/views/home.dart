37e5dbf15f58619b3f8e18816eb9b0006100a449
elysian12 <elysian1232@gmail.com>
2022-12-20 12:32:10 +0530

    [Added] setup google maps sdk

--- lib/modules/home/views/home.dart
+++ lib/modules/home/views/home.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String routeName = "/home";
 
-   static const String routeName = "/home";
-
-  const HomeScreen  ({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return const Scaffold(
-      body: Center(
-        child: Text('Made with 💙 by Outshade'),
+    return const Scaffold(
+        body: GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: LatLng(22.307159, 73.181221),
+        zoom: 14.4746,
       ),
-    );
+    ));
   }
 }
